c98a08347ce77871435df06b6844028ba797e686
Chad Fennell <chadfennell@users.noreply.github.com>
2026-06-18 19:17:46 -0700

    removes dead code

--- internal/utils.go
+++
@@ -1,9 +0,0 @@
-package utils
-
-func MapStrToStr(arr []string, fn func(s string) string) []string {
-	var newArray = []string{}
-	for _, it := range arr {
-		newArray = append(newArray, fn(it))
-	}
-	return newArray
-}
